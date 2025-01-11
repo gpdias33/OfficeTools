f8deec0d030406c4d51df39b359628d5ac842dc6
gpdias33 <gpdias33@gmail.com>
2025-01-11 09:30:21 -0300

    Apaguei os arquivos a.txt e home.js usados para exemplo de estudo.

--- home.js
+++
@@ -1,9 +0,0 @@
-//Função criada na versão 1.0
-function logar(nomeUsuario){
-        return nomeUsuario
-}
-
-//Função criada na versão 2.0
-function mostraSobreNome(sobreNome){
-    return sobreNome
-}
